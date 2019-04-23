420425466d3ff3414ea8e772083c59b140825fab
Connie Lim <jlim13@binghamton.edu>
2019-04-23 04:04:22 -0400

    added goals

--- src/components/Home.js
+++ src/components/Home.js
@@ -2,8 +2,18 @@ import React from 'react'
 import { Link } from 'react-router-dom'
 import firebase from 'firebase'
 import '../index.css'
-import Button from 'react-bootstrap/Button';
+// import Button from 'react-bootstrap/Button';
 import Nav from 'react-bootstrap/Nav'
+import styled from 'styled-components'
+
+const Button = styled.button`
+  background: rgb(228,211,176);
+  color: rgb(117,117,117);
+  margin: 1em;
+  padding: 10px;
+  width: 100px;
+  border: none;
+`
 
 export default class Home extends React.Component {
   constructor() {
@@ -40,8 +50,15 @@ export default class Home extends React.Component {
           <h1>Welcome to LogChimp!</h1>
         </div> :
         <div>
-          <Button onClick={() => firebase.auth().signOut()}>Sign out</Button>
-          <h1>Welcome back {firebase.auth().currentUser.email}!</h1>
+          <div>
+            <Button onClick={() => firebase.auth().signOut()}>Sign out</Button>
+            <h3>Welcome back {firebase.auth().currentUser.email}!</h3>
+          </div>
+          <div className="goals">
+            <h2>Goals</h2>
+            <input type="text" placeholder="Goal" />
+            <Button>Add Goal</Button>
+          </div>
         </div>
       }
       </div>
